5f20597cab1ce1183f44c2a32701a7858dcb2de6
Saahil Ali <programmer290399@gmail.com>
2021-06-27 21:07:51 +0530

    operation : archive : added methods for tarfiles

--- dffml/operation/archive.py
+++ dffml/operation/archive.py
@@ -1,6 +1,7 @@
 import uuid
-from zipfile import ZipFile
+import tarfile
 from pathlib import Path
+from zipfile import ZipFile
 from typing import Optional
 
 from ..df.base import op
@@ -10,6 +11,7 @@ from ..df.types import Definition
 # definitions
 DIRECTORY = Definition(name="directory", primitive="str")
 ZIP_FILE = Definition(name="zip_file", primitive="str")
+TAR_FILE = Definition(name="tar_file", primitive="str")
 
 
 @op(
@@ -61,3 +63,54 @@ async def extract_zip_archive(
         output_directory_path = Path(input_file_path).parent.absolute()
     with ZipFile(input_file_path, "r") as zip:
         zip.extractall(output_directory_path)
+
+
+@op(
+    inputs={"input_directory_path": DIRECTORY, "output_file_path": TAR_FILE},
+    outputs={},
+)
+async def make_tar_archive(
+    input_directory_path: str, output_file_path: Optional[str] = None,
+):
+    """
+    Creates tar file of a directory.
+
+    Parameters
+    ----------
+    input_directory_path : str
+        Path to directory to be archived as a tarfile.
+    output_file_path : Optional[str], optional
+        If set output file would be saved to this path otherwise 
+        file is saved in the given directory, by default None
+    """
+    input_directory_path = Path(input_directory_path)
+    if output_file_path is None:
+        file_name = str(uuid.uuid4()) + ".tar"
+        output_file_path = input_directory_path.parent / file_name
+    with tarfile.open(output_file_path, mode="x") as tar:
+        for file in input_directory_path.rglob("*"):
+            tar.add(file, file.name)
+
+
+@op(
+    inputs={"input_file_path": TAR_FILE, "output_directory_path": DIRECTORY},
+    outputs={},
+)
+async def extract_tar_archive(
+    input_file_path: str, output_directory_path: Optional[str] = None,
+):
+    """
+    Extracts a given tar file.
+
+    Parameters
+    ----------
+    input_file_path : str
+        Path to the tar file
+    output_directory_path : Optional[str], optional
+        If set the file would be extracted to this path otherwise 
+        the file would be extracted in the same directory as source, by default None
+    """
+    if output_directory_path is None:
+        output_directory_path = Path(input_file_path).parent.absolute()
+    with tarfile.open(input_file_path, "r") as tar:
+        tar.extractall(output_directory_path)
