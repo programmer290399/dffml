a950841fcfd5ae02389b698297878a330c0c1bfe
Saahil Ali <programmer290399@gmail.com>
2021-06-30 10:03:59 +0530

    tests : operation : compression : Updated Test Cases

--- tests/operation/test_compression.py
+++ tests/operation/test_compression.py
@@ -4,12 +4,14 @@ from unittest.mock import patch, mock_open, call
 from .test_archive import create_dataflow
 
 from dffml import run
-from dffml.df.base import OperationException
 from dffml.util.asynctestcase import AsyncTestCase
 from dffml.operation.compression import (
-    compress,
-    de_compress,
-    UnsupportedCompressionFormatError,
+    gz_compress,
+    gz_decompress,
+    bz2_compress,
+    bz2_decompress,
+    xz_compress,
+    xz_decompress,
 )
 
 
@@ -45,11 +47,10 @@ class TestCompressionOperations(AsyncTestCase):
 
     async def test_create_gz(self):
         dataflow = create_dataflow(
-            compress,
+            gz_compress,
             {
                 "input_file_path": self.uncomressed_file_pth,
                 "output_file_path": self.compressed_file_pth(".gz"),
-                "file_format": ".gz",
             },
         )
         m_open = mock_open()
@@ -61,11 +62,10 @@ class TestCompressionOperations(AsyncTestCase):
 
     async def test_create_bz2(self):
         dataflow = create_dataflow(
-            compress,
+            bz2_compress,
             {
                 "input_file_path": self.uncomressed_file_pth,
                 "output_file_path": self.compressed_file_pth(".bz2"),
-                "file_format": ".bz2",
             },
         )
         m_open = mock_open()
@@ -77,11 +77,10 @@ class TestCompressionOperations(AsyncTestCase):
 
     async def test_create_xz(self):
         dataflow = create_dataflow(
-            compress,
+            xz_compress,
             {
                 "input_file_path": self.uncomressed_file_pth,
                 "output_file_path": self.compressed_file_pth(".xz"),
-                "file_format": ".xz",
             },
         )
         m_open = mock_open()
@@ -93,7 +92,7 @@ class TestCompressionOperations(AsyncTestCase):
 
     async def test_inflate_gz(self):
         dataflow = create_dataflow(
-            de_compress,
+            gz_decompress,
             {
                 "input_file_path": self.compressed_file_pth(".gz"),
                 "output_file_path": self.uncomressed_file_pth,
@@ -108,7 +107,7 @@ class TestCompressionOperations(AsyncTestCase):
 
     async def test_inflate_bz2(self):
         dataflow = create_dataflow(
-            de_compress,
+            bz2_decompress,
             {
                 "input_file_path": self.compressed_file_pth(".bz2"),
                 "output_file_path": self.uncomressed_file_pth,
@@ -123,7 +122,7 @@ class TestCompressionOperations(AsyncTestCase):
 
     async def test_inflate_xz(self):
         dataflow = create_dataflow(
-            de_compress,
+            xz_decompress,
             {
                 "input_file_path": self.compressed_file_pth(".xz"),
                 "output_file_path": self.uncomressed_file_pth,
@@ -135,30 +134,3 @@ class TestCompressionOperations(AsyncTestCase):
         ), patch("shutil.copyfileobj"):
             async for _, _ in run(dataflow):
                 m_open.assert_has_calls(self.get_inflation_mock_calls(".xz"))
-
-    async def test_compress_raises_UnsupportedCompressionFormatError(self):
-        dataflow = create_dataflow(
-            compress,
-            {
-                "input_file_path": self.uncomressed_file_pth,
-                "output_file_path": self.compressed_file_pth(".xyz"),
-                "file_format": ".xyz",
-            },
-        )
-        with self.assertRaises(OperationException):
-            with self.assertRaises(UnsupportedCompressionFormatError):
-                async for _, _ in run(dataflow):
-                    pass
-
-    async def test_de_compress_raises_UnsupportedCompressionFormatError(self):
-        dataflow = create_dataflow(
-            de_compress,
-            {
-                "input_file_path": self.compressed_file_pth(".xyz"),
-                "output_file_path": self.uncomressed_file_pth,
-            },
-        )
-        with self.assertRaises(OperationException):
-            with self.assertRaises(UnsupportedCompressionFormatError):
-                async for _, _ in run(dataflow):
-                    pass
