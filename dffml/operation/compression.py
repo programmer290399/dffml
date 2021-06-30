093ff3e183fb21e380c5cc076d6f284ba18f3402
Saahil Ali <programmer290399@gmail.com>
2021-06-30 09:58:53 +0530

    operation : compression : made individual compress/decompress functions for each format

--- dffml/operation/compression.py
+++ dffml/operation/compression.py
@@ -1,3 +1,4 @@
+import sys
 import bz2
 import gzip
 import lzma
@@ -7,80 +8,73 @@ from pathlib import Path
 from ..df.base import op
 from ..df.types import Definition
 
-# definitions
-INPUT_FILE_PATH = Definition(name="input_file_path", primitive="str")
-OUTPUT_FILE_PATH = Definition(name="output_file_path", primitive="str")
-FORMAT = Definition(name="format", primitive="str")
 
-SUPPORTED_COMPRESSION_FORMATS = {".gz": gzip, ".bz2": bz2, ".xz": lzma}
+def make_compress(extension, compression_cls):
+    async def compress(
+        input_file_path: str, output_file_path: str,
+    ):
+        f"""
+        A simple function to compress a {extension} file.
 
+        Parameters
+        ----------
+        input_file_path : str
+            Path of the file to be compressed.
+        output_file_path : str
+            Path where the output should be saved (should include file name).
+        """
+        with open(input_file_path, "rb") as f_in:
+            with compression_cls.open(output_file_path, "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
 
-class UnsupportedCompressionFormatError(Exception):
-    def __init__(self, format):
-        super().__init__()
-        self.format = format
-
-    def __str__(self):
-        return f"{self.format} format is not currently supported."
-
-
-def get_compression_class(format):
-    compression_cls = SUPPORTED_COMPRESSION_FORMATS.get(format, None)
-    if compression_cls is None:
-        raise UnsupportedCompressionFormatError(format)
-    return compression_cls
+    return compress
 
 
-@op(
-    inputs={
-        "input_file_path": INPUT_FILE_PATH,
-        "output_file_path": OUTPUT_FILE_PATH,
-        "file_format": FORMAT,
-    },
-    outputs={},
-)
-async def compress(
-    input_file_path: str, output_file_path: str, file_format: str
-):
-    """
-    A simple function to compress a file in a certain format.
+def make_decompress(extension, compression_cls):
+    async def decompress(input_file_path: str, output_file_path: str):
+        f"""
+        A simple function to decompress a {extension} file.
 
-    Parameters
-    ----------
-    input_file_path : str
-        Path of the file to be compressed.
-    output_file_path : str
-        Path where the output should be saved (should include file name).
-    file_format : str
-        Format of the compressed output file.
-    """
-    compression_cls = get_compression_class(file_format)
-    with open(input_file_path, "rb") as f_in:
-        with compression_cls.open(output_file_path, "wb") as f_out:
-            shutil.copyfileobj(f_in, f_out)
+        Parameters
+        ----------
+        input_file_path : str
+            Path of the file to be decompressed.
+        output_file_path : str
+            Path where the output should be saved (should include file name).
+        """
+        input_file_path = Path(input_file_path)
+        with compression_cls.open(input_file_path, "rb") as f_in:
+            with open(output_file_path, "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+    return decompress
 
 
-@op(
-    inputs={
-        "input_file_path": INPUT_FILE_PATH,
-        "output_file_path": OUTPUT_FILE_PATH,
-    },
-    outputs={},
-)
-async def de_compress(input_file_path: str, output_file_path: str):
-    """
-    A simple function to de_compress a file.
+SUPPORTED_COMPRESSION_FORMATS = {"gz": gzip, "bz2": bz2, "xz": lzma}
 
-    Parameters
-    ----------
-    input_file_path : str
-        Path of the file to be decompressed.
-    output_file_path : str
-        Path where the output should be saved (should include file name).
-    """
-    input_file_path = Path(input_file_path)
-    file_format = input_file_path.suffix
-    compression_cls = get_compression_class(file_format)
-    with compression_cls.open(input_file_path, "rb") as f_in:
-        with open(output_file_path, "wb") as f_out:
-            shutil.copyfileobj(f_in, f_out)
+for extension, compression_cls in SUPPORTED_COMPRESSION_FORMATS.items():
+    # Create definitions for compressed/decompressed file path for this format
+    compressed_file_path = Definition(
+        name=f"compressed_{extension}_file_path", primitive="str"
+    )
+    decompressed_file_path = Definition(
+        name=f"decompressed_{extension}_file_path", primitive="str"
+    )
+
+    compress = op(
+        inputs={
+            "input_file_path": decompressed_file_path,
+            "output_file_path": compressed_file_path,
+        },
+        outputs={},
+    )(make_compress(extension, compression_cls))
+    decompress = op(
+        inputs={
+            "input_file_path": compressed_file_path,
+            "output_file_path": decompressed_file_path,
+        },
+        outputs={},
+    )(make_decompress(extension, compression_cls))
+
+    setattr(sys.modules[__name__], f"{extension}_compress", compress)
+    setattr(sys.modules[__name__], f"{extension}_decompress", decompress)
